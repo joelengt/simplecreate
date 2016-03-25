41666d7d47f85a4693db0f7326eb8fc2425919fc
Joel Gonzales Tipismana <joelengt@gmail.com>
2016-03-24 22:03:21 -0500

    archivo config.js añadido para deploy

---
+++ config.js
@@ -0,0 +1,17 @@
+var config  = {
+	mongodb: {
+		localhost: 'mongodb://localhost/personajes',
+		mlab: 'mongodb://singers:singers@ds019829.mlab.com:19829/singers-simple'
+	},
+	cloudinary: {
+		cloud_name: 'cromlu',
+		api_key: '532668554832195',
+		api_secret: 'PLstoVjJNoBiqPhNDGriHyVWVTc'
+	},
+	admin: {
+		name: 'admin',
+		pass: '12345678'
+	}
+}
+
+module.exports = config
